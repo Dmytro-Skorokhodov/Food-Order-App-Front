a7c1e7dc6e9d8f7146e24c1319e7f27e3b98d700
Дмитро Скороходов <dimon.skorohodov@gmail.com>
2024-03-01 22:15:40 +0200

    1

--- src/components/meals/Meal.jsx
+++ src/components/meals/Meal.jsx
@@ -1,27 +1,27 @@
-const components = import.meta.glob("../../assets/*.jpg");
-const componentsKeys = Object.keys(components);
+// const components = import.meta.glob("../../assets/*.jpg");
+// const componentsKeys = Object.keys(components);
 
 
-let x = [];
+// let x = [];
   
 
-for (const path in components) {
-  const component = components[path]();
-  x.push(component);
-  console.log(component.default);
-}
+// for (const path in components) {
+//   const component = components[path]();
+//   x.push(component);
+//   console.log(component.default);
+// }
 
 
 
 export default function Meal({ name, price, description, image, onAddToCart }) {
   
   // const url = componentsKeys.filter((comp) => comp === image);
-  const url = x[0].default;
+  // const url = x[0].default;
   
 
   return (
     <article className="meal-item">
-      <img src={url} alt="food" />
+      <img src={image} alt="food" />
       <h3>{name}</h3>
       <div className="meal-item-price">${price}</div>
       <p className="meal-item-description">{description}</p>

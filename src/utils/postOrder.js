07e9ef644f51e44a79290009b113e043de5cb024
Дмитро Скороходов <dimon.skorohodov@gmail.com>
2024-03-06 19:03:58 +0200

    added no-cors policy to post request

--- src/utils/postOrder.js
+++ src/utils/postOrder.js
@@ -2,6 +2,7 @@
 export async function postOrder(order) {
   const response = await fetch("https://food-order-app-backend-smoky.vercel.app/orders", {
     method: "POST",
+    mode: "no-cors",
     body: JSON.stringify({ order }),
     headers: {
       "Content-Type": "application/json",

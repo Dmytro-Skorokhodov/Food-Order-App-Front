746ab64811785773fd5dac3e058275934b36278e
Дмитро Скороходов <dimon.skorohodov@gmail.com>
2024-03-07 00:11:06 +0200

    12

--- src/components/Modal/Orders.jsx
+++ src/components/Modal/Orders.jsx
@@ -13,6 +13,8 @@ export default function Orders({ onCloseModal }) {
           "https://food-order-app-backend-smoky.vercel.app/orders"
         );
 
+        console.log(response);
+
         const resData = await response.json();
 
         if (!response.ok) {

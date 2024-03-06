ae29ede466a1f5177c6d1024ab08147c0e54638e
Дмитро Скороходов <dimon.skorohodov@gmail.com>
2024-03-06 23:07:03 +0200

    12

--- src/utils/postOrder.js
+++ src/utils/postOrder.js
@@ -3,7 +3,7 @@ export async function postOrder(order) {
     "https://food-order-app-backend-git-b1b21c-dmytro-skorokhodovs-projects.vercel.app/orders",
     {
       method: "POST",
-      body: JSON.stringify({ order }),
+      body:  order,
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "https://food-order-app-front.vercel.app",

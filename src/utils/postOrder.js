9448e7d4ea0d5ef6e88ec5f4ce9c98c5948f8151
Дмитро Скороходов <dimon.skorohodov@gmail.com>
2024-03-06 19:09:10 +0200

    added ACAO in headers

--- src/utils/postOrder.js
+++ src/utils/postOrder.js
@@ -1,12 +1,15 @@
-
 export async function postOrder(order) {
-  const response = await fetch("https://food-order-app-backend-git-b1b21c-dmytro-skorokhodovs-projects.vercel.app/orders", {
-    method: "POST",
-    body: JSON.stringify({ order }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const response = await fetch(
+    "https://food-order-app-backend-git-b1b21c-dmytro-skorokhodovs-projects.vercel.app/orders",
+    {
+      method: "POST",
+      body: JSON.stringify({ order }),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "https://food-order-app-front.vercel.app",
+      },
+    }
+  );
 
   const data = await response.json();
 
